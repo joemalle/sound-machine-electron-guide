0484fe4429053426965ed24a9f1f84941f30b9b7
Joseph Malle <j.e.f.malle@gmail.com>
2018-02-27 22:46:38 -0500

    updated 01

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+const {app, BrowserWindow} = require('electron');
 
 var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
+        height: 700,
+        width: 368
     });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
 });
