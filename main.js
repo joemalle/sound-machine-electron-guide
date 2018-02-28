7ad46db5ea673c71c079bf7eaa77c81e686c66c9
Joseph Malle <j.e.f.malle@gmail.com>
2018-02-27 22:49:10 -0500

    fixed 02

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+const {app, BrowserWindow, ipcMain, globalShortcut} = require('electron');
 
 var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        frame: false,
         height: 700,
-        resizable: false,
-        width: 368
+        width: 368,
+        frame: false,
+        resizable: false
     });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
 });
